174149575bb5eee2742a3d0acbeb14d2c122d66e
Sakib Alamin <sakibalamin162@gmail.com>
2020-08-05 16:01:17 +0600

    added fastIO

--- tictactoe-minimax.cpp
+++ tictactoe-minimax.cpp
@@ -102,6 +102,7 @@ pair <int,int> findBestPos() {
     return make_pair(l, r);
 }
 int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
     setWinningState();
     memset(board, '*', sizeof(board));
     printBoard();
